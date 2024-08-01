fbc729f1f8e546167bfbd7b9d6d79af753aa5421
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-08-01 21:45:34 +0300

    ref: children for button and add function to toggle logo

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,13 +2,13 @@ import style from './Button.module.css';
 import cn from 'classnames';
 
 
-export function Button({ text, onClick, isAccent }) {
+export function Button({ children = 'Сохранить', onClick, isAccent }) {
 	return (
 		<button
 			className={cn(style.button, {[style.accent]: isAccent})}
 			onClick={onClick}
 		>
-			{text}
+			{children}
 		</button>
 	);
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import styles from './Header.module.css';
 import SelectUser from '../SelectUser/SelectUser.jsx';
+import {useState} from 'react';
+import {Button} from '../Button/Button.jsx';
+
+const LOGO_LIST = ['vite.svg', 'cloud-rain.svg'];
+
 
 function Header() {
+	const [logoIdx, setLogoIdx] = useState(0);
+
+	const toggleLogo = () => {
+		setLogoIdx((prev) => Number(!prev));
+	};
+
 	return (
 		<>
-			<img className={styles['logo']} src="/vite.svg" alt="logo"/>
+			<img className={styles['logo']} src={LOGO_LIST[logoIdx]} alt="logo"/>
 			<SelectUser />
+			<Button isAccent onClick={toggleLogo}>Изменить логотип</Button>
 		</>
 	);
 }
