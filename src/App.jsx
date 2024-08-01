d4f81101949302d5bae629ba0181c99b5f38325e
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-08-01 16:46:45 +0300

    ref: change style and condition of data processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,33 @@
 import { wrapper } from './App.module.css';
-import JournalItem from './components/JournalItem/JournalItem.jsx';
-import CardButton from './components/CardButton/CardButton.jsx';
 import LeftPanel from './layouts/LeftPanel/LeftPanel.jsx';
 import Body from './layouts/Body/Body.jsx';
 import Header from './components/Header/Header.jsx';
 import JournalList from './components/JournalList/JournalList.jsx';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx';
 import JournalForm from './components/JournalForm/JournalForm.jsx';
-import {useEffect, useState} from 'react';
+import {useLocaleStorageHook} from './hooks/useLocaleStorage.hook.js';
+
+function mapItems (items){
+	if (!items) {
+		return [];
+	}
+	
+	return items.map((i) => ({
+		...i,
+		date: new Date(i.date)
+	}));
+}
 
 function App() {
-	const [data, setData] = useState([]);
-
-	useEffect(() => {
-		const dataFromLocalStorage = JSON.parse(localStorage.getItem('data'));
-
-		if (dataFromLocalStorage) {
-			setData(dataFromLocalStorage.map((journal) => ({
-				...journal,
-				date: new Date(journal.date)
-			})));
-		}
-	}, []);
-
-	useEffect(() => {
-		if (data.length) {
-			localStorage.setItem('data', JSON.stringify(data));
-		}
-	}, [data]);
+	const [data, setData] = useLocaleStorageHook('data');
 
 	const addItem = (item) => {
-		setData((prev) => [
-			...prev,
-			{
-				title: item.title,
-				text: item.text,
-				date: new Date(item.date),
-				id: +prev.length + 1
-			}
-		]);
-	};
-
-	const sortItems = (a, b) => {
-		if (a.date < b.date) {
-			return 1;
-		}
-
-		return -1;
+		setData([...mapItems(data), {
+			title: item.title,
+			text: item.text,
+			date: new Date(item.date),
+			id: +data.length + 1
+		}]);
 	};
 
 	return (
@@ -54,23 +35,10 @@ function App() {
 			<LeftPanel>
 				<Header/>
 				<JournalAddButton/>
-				<JournalList>
-					{ data.length === 0 && <p>Записей пока нет. Добавьте новую запись</p> }
-					{
-						data.length > 0 && data.sort(sortItems).map((item) => (
-							<CardButton key={item.id}>
-								<JournalItem
-									title={item.title}
-									text={item.text}
-									date={item.date}
-								/>
-							</CardButton>
-						))
-					}
-				</JournalList>
+				<JournalList items={mapItems(data)} />
 			</LeftPanel>
 			<Body>
-				<JournalForm onSubmit={addItem} />
+				<JournalForm onSubmit={addItem}/>
 			</Body>
 		</div>
 	);
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,11 +1,32 @@
 import style from './JournalList.module.css';
+import CardButton from '../CardButton/CardButton.jsx';
+import JournalItem from '../JournalItem/JournalItem.jsx';
 
-function JournalList({ children }) {
+function JournalList({ items }) {
+	if (!items || items.length === 0) {
+		return <p>Записей пока нет. Добавьте новую запись</p>;
+	}
+
+	const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		}
+
+		return -1;
+	};
+	
 	return (
 		<div className={style['journal-list']}>
-			{children}
+			{items.sort(sortItems).map((item) => (
+				<CardButton key={item.id}>
+					<JournalItem
+						title={item.title}
+						text={item.text}
+						date={item.date}
+					/>
+				</CardButton>
+			))}
 		</div>
-
 	);
 }
 
